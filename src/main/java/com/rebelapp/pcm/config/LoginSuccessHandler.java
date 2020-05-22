b131acd442ae976f9cf01fe2fe1d66d629e21e84
Okamoto Yuusuke <yuusuke_mbp@OkamotonoMacBook-Pro.local>
2020-05-22 12:16:05 +0900

    add files

---
+++ src/main/java/com/rebelapp/pcm/config/LoginSuccessHandler.java
@@ -0,0 +1,24 @@
+package com.rebelapp.pcm.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+
+@Configuration
+public abstract class LoginSuccessHandler implements AuthenticationFailureHandler {
+	
+	
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication ) 
+		throws IOException, ServletException
+	{
+		response.sendRedirect("/");
+	}
+	
+}
