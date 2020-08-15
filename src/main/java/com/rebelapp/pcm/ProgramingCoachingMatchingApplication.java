b1bda2238b2d157c9a05fc897cded954f2399ddc
kazuya.sasaki <kazuya.sasaki39@gmail.com>
2020-08-15 20:49:56 +0900

    repair for deploy to tomcat

--- src/main/java/com/rebelapp/pcm/ProgramingCoachingMatchingApplication.java
+++ src/main/java/com/rebelapp/pcm/ProgramingCoachingMatchingApplication.java
@@ -3,13 +3,20 @@ package com.rebelapp.pcm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.rebelapp.pcm.entity")
-public class ProgramingCoachingMatchingApplication {
+public class ProgramingCoachingMatchingApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramingCoachingMatchingApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProgramingCoachingMatchingApplication.class);
+    }
 
 }
